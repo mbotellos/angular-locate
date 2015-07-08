145e6e8b3c2d2173c910f1399a149132505beea1
mbotellos <mbotellos@hotmail.com>
2015-07-08 11:36:04 -0500

    FIRST COMMIT - yeoman angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMultiLanguageProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularMultiLanguageProjectApp
+ */
+angular.module('angularMultiLanguageProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMultiLanguageProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularMultiLanguageProjectApp
+ */
+angular.module('angularMultiLanguageProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
